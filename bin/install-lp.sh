d667cae384f3a100087f53e0d4738afbd5ba13cb
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2019-04-16 16:29:35 +0700

    Simplify landing extraction

--- bin/install-lp.sh
+++ bin/install-lp.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-sudo -u keitaro mkdir -p -m 777 /var/www/keitaro/landers/local-land
-curl -sL https://github.com/apliteni/keitaro-benchmark/raw/master/datasets/lp.zip > /var/www/keitaro/landers/local-land/lp.zip
-cd  /var/www/keitaro/landers/local-land/
-sudo -u keitaro unzip lp.zip
+sudo -u keitaro bash -c 'curl -fsL https://github.com/apliteni/keitaro-benchmark/raw/master/datasets/landers.tar.gz | tar -xzC /var/www/keitaro/'
 sudo -u keitaro php /var/www/keitaro/bin/cli.php  system:reload_cache
